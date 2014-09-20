b55d9d1341018601d6fd90e9ed7299199fd81c05
Dongsu Jang <iolothebard@gmail.com>
2014-09-20 23:46:41 +0900

    remove personal error and add error class buider

--- tests/errors_test.js
+++ tests/errors_test.js
@@ -16,9 +16,11 @@ describe('errors', function () {
         assert.ok(require('util').isError(e));
         assert.equal(e.name, 'Error');
         assert.equal(e.message, 'foo');
+        assert.equal(e.toString(), 'Error: foo');
+        assert.equal(JSON.stringify(e), JSON.stringify({}));
     });
     it('CustomError', function () {
-        var e = new errors.CustomError(1234, 'foo');
+        var e = new errors.CustomError(1234, 'foo', {cause: 4567});
         debug('error:', e);
         debug('toString:', e.toString());
         debug('message:', e.message);
@@ -26,6 +28,39 @@ describe('errors', function () {
         assert.ok(e instanceof errors.CustomError);
         assert.ok(e instanceof Error);
         //FIXME: test.ok(require('util').isError(e));
+        assert.equal(e.name, 'CustomError');
         assert.equal(e.code, 1234);
+        assert.equal(e.message, 'foo');
+        assert.deepEqual(e.cause, {cause: 4567});
+        assert.equal(e.toString(), 'CustomError#1234: foo')
+        assert.equal(JSON.stringify(e), JSON.stringify({
+            name: 'CustomError',
+            code: 1234,
+            message: 'foo',
+            cause: {cause: 4567}
+        }));
+    });
+    it('defineError', function () {
+        var MyError = errors.defineError('MyError', errors.CustomError);
+        var e = new MyError(1234, 'foo', {cause: 4567});
+        debug('error:', e);
+        debug('toString:', e.toString());
+        debug('message:', e.message);
+        debug('name:', e.name);
+        assert.ok(e instanceof MyError);
+        assert.ok(e instanceof errors.CustomError);
+        assert.ok(e instanceof Error);
+        //FIXME: test.ok(require('util').isError(e));
+        assert.equal(e.name, 'MyError');
+        assert.equal(e.code, 1234);
+        assert.equal(e.message, 'foo');
+        assert.deepEqual(e.cause, {cause: 4567});
+        assert.equal(e.toString(), 'MyError#1234: foo')
+        assert.equal(JSON.stringify(e), JSON.stringify({
+            name: 'MyError',
+            code: 1234,
+            message: 'foo',
+            cause: {cause: 4567}
+        }));
     });
 });
